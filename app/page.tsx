3530d0370fb7ddc31ee640358d7adcb8720500ca
krishnavamshikv <valupadasukrishnavamshi@gmail.com>
2025-03-22 04:11:28 +0530

    Tailwindcss updated in global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <h1 className="text-3xl text-green-600">Hello world from next Js</h1>;
+  return (
+    <h1 className="text-3xl text-primary-500">Hello world from next Js</h1>
+  );
 }
