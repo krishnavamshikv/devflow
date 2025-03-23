26a842c7aa3e0b5279e8ca95d36947794c7e4823
krishnavamshikv <valupadasukrishnavamshi@gmail.com>
2025-03-23 14:13:23 +0530

    Dark Theme using nextThemes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-space-grotesk">Hello from Next JS</h1>
-      <h1 className="text-3xl ">Hello from Next JS</h1>
+      <h1 className="font-space-grotesk text-3xl">Hello from Next JS</h1>
+      <h1 className="text-3xl font-black">Hello from Next JS</h1>
     </>
   );
 }
---
+++ context/theme.tsx
@@ -0,0 +1,10 @@
+import {
+  ThemeProviderProps,
+  ThemeProvider as NextThemeProvider,
+} from "next-themes";
+
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+};
+
+export default ThemeProvider;
