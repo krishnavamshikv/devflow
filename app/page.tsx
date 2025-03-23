4abc251d62e1e85dbc865628a5aeafe9d278e5ef
krishnavamshikv <valupadasukrishnavamshi@gmail.com>
2025-03-23 09:51:54 +0530

    Assets & Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-red-600 font-black ">Hello world! </h1>
+      <h1 className="text-3xl font-space-grotesk">Hello from Next JS</h1>
+      <h1 className="text-3xl ">Hello from Next JS</h1>
     </>
   );
 }
